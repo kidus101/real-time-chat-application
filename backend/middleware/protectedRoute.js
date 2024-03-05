571f5996354d31d9ce1cacb74e56047442b4fce8
kidus <69918484+kidus101@users.noreply.github.com>
2024-03-05 10:59:12 +0300

    Adding the Protected routes

---
+++ backend/middleware/protectedRoute.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+const protectedRoute = async (req, res) => {
+  const token = req.cookie.jwt;
+
+  if (!token) {
+    res.status(401).json({ error : "UNAUTHORIZED TOKEN NOT PROVIDED" });
+  }
+
+  const decodedToken = await jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+  if (!decodedToken) {
+    res
+      .status(401)
+      .json({
+        error : "Unauthorized - Invalid Token",
+      });
+  }
+
+  console.log(decodedToken);
+
+  const user = await User.findById(decodedToken.user_id).select("-password");
+
+  if(!user){
+    res.status(400).json({ error : "NO USER FOUND" });
+  }
+
+  req.user = user;
+
+  next();
+
+
+
+};
+
+export default protectedRoute;
