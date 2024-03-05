b5083f230d38a74d57cbf663bb90dedb83ce951b
kidus <69918484+kidus101@users.noreply.github.com>
2024-03-05 10:58:42 +0300

    Refactoring server.js and the auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import generateWebToken from "../util/generateToken.js";
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
+import messageRoute from "./routes/messageRoute.js";
+import cookieParser from "cookie-parser";
 import connnectToMongoDb from "./db/mongoDbConnect.js";
 import dotenv from "dotenv";
 
@@ -9,7 +11,10 @@ const PORT = 5000;
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoute);
+
 
 app.listen(PORT, () => {
   connnectToMongoDb();
